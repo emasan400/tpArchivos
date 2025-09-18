ccba9ffd022fa5e3001208a1e990ab199068842f
S Nemi <seba.nemi16@gmail.com>
2025-09-18 11:42:40 -0300

    Codigo seba

--- src/demo/RegData.java
+++ src/demo/RegData.java
@@ -1,4 +1,5 @@
-package demo;/*
+package demo;
+/*
  * Lista que guarde el mapa de las personas:
  * Hacer un map con info de las personas junto a su tamaño
  * En el archivo para que lea bien el dato despues (limite de puntero)
